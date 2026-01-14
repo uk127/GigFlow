6f0ddda966f6690b3af81d0ffcdaded493d0ec5f
Unni Krishnan B V <unnik9218@gmail.com>
2026-01-14 21:19:58 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,9 +27,9 @@ export const register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite:  process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000
+      secure: true,            // Must be HTTPS
+      sameSite: 'none',        // Cross-site cookies
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.status(201).json({
@@ -64,9 +64,9 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000
+      secure: true,       // HTTPS only
+      sameSite: 'none',   // Cross-site
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.json({
@@ -82,8 +82,8 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.cookie('token', '', {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     expires: new Date(0)
   });
 
